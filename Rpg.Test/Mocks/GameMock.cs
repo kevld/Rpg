df6f2ef6ae4220fc3333468b0014884342e6aeb5
kevld <kevin.viroulaud@outlook.com>
2024-11-18 21:21:33 -0500

    test

--- Rpg.Test/Mocks/GameMock.cs
+++ Rpg.Test/Mocks/GameMock.cs
@@ -10,18 +10,24 @@ namespace Rpg.Test.Mocks
         public GameMock()
         {
             Content.RootDirectory = "Content";
+            Console.WriteLine("aa");
             GraphicsAdapter.UseReferenceDevice = true;
             GraphicsAdapter.UseDriverType = GraphicsAdapter.DriverType.FastSoftware;
             GraphicsDeviceManager = new GraphicsDeviceManager(this);
-            
+            Console.WriteLine("aab");
+
         }
 
         public void InitializeOnly()
         {
             if (GraphicsDevice == null)
             {
+                Console.WriteLine("aac");
+
                 var graphicsDeviceManager = Services.GetService(typeof(IGraphicsDeviceManager)) as IGraphicsDeviceManager;
+            Console.WriteLine("aad");
                 graphicsDeviceManager?.CreateDevice();
+            Console.WriteLine("aae");
             }
             Initialize();
         }
